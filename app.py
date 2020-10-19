355cda54cea2ba05b0900f6925635c049f44c0dd
alishahwee <alisha.hwee@outlook.com>
2020-10-18 22:19:58 -0500

    Load environment from dotenv

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template
+from dotenv import load_dotenv
 import os
+
+# Load environment variables
+load_dotenv()
 
 app = Flask(__name__)
 
 @app.route("/map")
 def map():
-    return render_template("map.html", token = os.environ["MAPBOX_TOKEN"])
+    """Render mapbox map."""
+    
+    return render_template("map.html", token = os.environ.get("MAPBOX_TOKEN"))
