d29815cfad70eb3bb1362dba11f7eabab3b0a4d7
alishahwee <alisha.hwee@outlook.com>
2020-10-21 14:35:57 -0500

    Update models to include images and activities

--- models.py
+++ models.py
@@ -1,16 +1,31 @@
+from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
 
 meta = MetaData()
 
-engine = create_engine("postgresql:///camping", echo=True)
+engine = create_engine("postgresql:///state_parks", echo=True)
 
-state_parks = Table(
-    "state_parks",
+parks = Table(
+    "parks",
     meta,
     Column("id", Integer, primary_key=True),
     Column("name", String, nullable=False),
     Column("address", String, nullable=False),
     Column("url", String, nullable=False),
+)
+
+images = Table(
+    "images",
+    meta,
+    Column("parks_id", Integer, ForeignKey("parks.id"), nullable=False),
+    Column("url", String),
+)
+
+activities = Table(
+    "activities",
+    meta,
+    Column("parks_id", Integer, ForeignKey("parks.id"), nullable=False),
+    Column("activity", String),
 )
 
 if __name__ == "__main__":
