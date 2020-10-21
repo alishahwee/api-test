9446e96c28d77b27341e194739a9fb332dcbe4a5
alishahwee <alisha.hwee@outlook.com>
2020-10-20 19:07:45 -0500

    Create template for input

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from dotenv import load_dotenv
 import os
 
@@ -7,8 +7,16 @@ load_dotenv()
 
 app = Flask(__name__)
 
+
 @app.route("/map")
 def map():
     """Render mapbox map."""
-    
-    return render_template("map.html", token = os.environ.get("MAPBOX_TOKEN"))
+
+    return render_template("map.html", token=os.environ.get("MAPBOX_TOKEN"))
+
+
+@app.route("/campgrounds")
+def campgrounds():
+    """Returns a list of campgrounds."""
+
+    return render_template("campgrounds.html")
