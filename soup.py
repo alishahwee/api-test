50a4ff738749f17912c3fb089f361e27441a3908
alishahwee <alisha.hwee@outlook.com>
2020-10-20 23:04:56 -0500

    Dump data into a json file

--- soup.py
+++ soup.py
@@ -1,7 +1,12 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+import json
 
+# from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
+
+# db = create_engine("postgresql+psycopg2://alishahwee@localhost:5432/parks", echo=True)
+# meta = MetaData()
 
 parks_url = "https://www.dnr.state.mn.us"
 
@@ -27,16 +32,34 @@ for link in soup.select(".list a"):
         images = [img.get("src") for img in soup_page.select("#thumbs img")]
         images.append(soup_page.find(class_="page_banner").get("src"))
         text_list = [
-            text for text in soup_page.select_one("#park .row .col-sm-8 p").stripped_strings
+            text
+            for text in soup_page.select_one("#park .row .col-sm-8 p").stripped_strings
         ]
         address = f"{text_list[0]} {text_list[1]}"
-        activities = [activity.string for activity in soup_page.select("#park .row ul li")]
+        activities = [
+            activity.string for activity in soup_page.select("#park .row ul li")
+        ]
         parks.append(
-            dict(name=name, address=address, activities=activities, url=url, images=images)
+            dict(
+                name=name,
+                address=address,
+                activities=activities,
+                url=url,
+                images=images,
+            )
         )
     except:
         continue
 
 driver.close()
 
-print(parks)
+with open("parks.json", "w") as outfile:
+    json.dump(parks, outfile)
+
+# mn_state_parks = Table(
+#     "mn_state_parks", meta,
+#     Column("id", Integer, primary_key=True),
+#     Column("name", String, nullable=False),
+#     Column("address", String, nullable=False),
+#     Column("url", String, nullable=False)
+# )
