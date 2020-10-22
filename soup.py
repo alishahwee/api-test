987576078e45cc494349d6ac40865c06ed9f9434
alishahwee <alisha.hwee@outlook.com>
2020-10-21 23:05:08 -0500

    Fix content loading issues and scrape coordinates

--- soup.py
+++ soup.py
@@ -1,5 +1,8 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 import json
 
@@ -21,8 +24,14 @@ for link in soup.select(".list a"):
 
     try:
         name = link.string
+        if "Recreation Area" in name:
+            print(f"Skip {name}")
+            continue
         url = parks_url + link.get("href")
         driver.get(parks_url + link.get("href"))
+        WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, ".page_banner"))
+        )
         soup_page = BeautifulSoup(driver.page_source, "html5lib")
         images = [img.get("src") for img in soup_page.select("#thumbs img")]
         images.insert(0, soup_page.find(class_="page_banner").get("src"))
@@ -31,6 +40,11 @@ for link in soup.select(".list a"):
             for text in soup_page.select_one("#park .row .col-sm-8 p").stripped_strings
         ]
         address = f"{text_list[0]} {text_list[1]}"
+        lat_long = (
+            soup_page.select_one("#park .row .col-sm-8 small")
+            .a.next_sibling.next_sibling.get("href")
+            .split("=")[1]
+        )
         activities = [
             activity.string for activity in soup_page.select("#park .row ul li")
         ]
@@ -38,12 +52,14 @@ for link in soup.select(".list a"):
             dict(
                 name=name,
                 address=address,
+                lat_long=lat_long,
                 activities=activities,
                 url=url,
                 images=images,
             )
         )
-    except:
+    except Exception as e:
+        print(f"Current page: {url}, {e}")
         continue
 
 driver.close()
