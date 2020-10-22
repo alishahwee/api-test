11fbfc24e8afb357e49469e942242bb27b990361
alishahwee <alisha.hwee@outlook.com>
2020-10-21 21:22:24 -0500

    Build function to insert data from json file

--- models.py
+++ models.py
@@ -1,11 +1,14 @@
+"""Build our models and insert data from json."""
+
 from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
+import json
 
 meta = MetaData()
 
 engine = create_engine("postgresql:///state_parks", echo=True)
 
-parks = Table(
+parks_table = Table(
     "parks",
     meta,
     Column("id", Integer, primary_key=True),
@@ -14,19 +17,51 @@ parks = Table(
     Column("url", String, nullable=False),
 )
 
-images = Table(
+images_table = Table(
     "images",
     meta,
     Column("parks_id", Integer, ForeignKey("parks.id"), nullable=False),
     Column("url", String),
 )
 
-activities = Table(
+activities_table = Table(
     "activities",
     meta,
     Column("parks_id", Integer, ForeignKey("parks.id"), nullable=False),
     Column("activity", String),
 )
 
+
+def insert_parks(json_file):
+    """Decode json and insert into database."""
+
+    with open(json_file) as f:
+        parks = json.load(f)
+
+        for park in parks:
+            name = park["name"]
+            address = park["address"]
+            activities = park["activities"]
+            url = park["url"]
+            images = park["images"]
+
+            ins = parks_table.insert().values(name=name, address=address, url=url)
+            parks_id = db.execute(ins).inserted_primary_key[0]
+            db.execute(
+                images_table.insert(),
+                [{"parks_id": parks_id, "url": img_url} for img_url in images],
+            )
+            db.execute(
+                activities_table.insert(),
+                [
+                    {"parks_id": parks_id, "activity": activity}
+                    for activity in activities
+                ],
+            )
+
+
 if __name__ == "__main__":
+    db = engine.connect()
     meta.create_all(engine)
+    insert_parks("parks.json")
+    db.close()
